f3544410be18128d22c67b3a55d91474d5775a49
Alexey Belyakov <albelyakov@griddynamics.com>
2019-07-10 18:25:41 +0400

    Add map rendering draft; Simulation classes bug fixes;

--- src/envs/wh_objects.py
+++ src/envs/wh_objects.py
@@ -1,8 +1,8 @@
 class Product(object):
-    def __init__(self, weight, volume, name, manufacturer, price):  # , count=1):
+    def __init__(self, name, weight, volume, manufacturer, price):  # , count=1):
+        self.name = name
         self.weight = weight
         self.volume = volume
-        self.name = name
         self.manufacturer = manufacturer
         self.price = price
         # self.count = count
@@ -51,7 +51,7 @@ class Shelf(object):
 
 class Agent(object):
 
-    def __init__(self, name, max_volume, max_weight, coordinates=(0,0)):
+    def __init__(self, name='Bill', max_volume=30, max_weight=30, coordinates=(1,1)):
         self.name = name
         self.coordinates = coordinates
         self.max_volume = max_volume
@@ -60,36 +60,46 @@ class Agent(object):
         self.available_load = max_weight
         self.inventory = dict()
         self.origin = "agent"
-        self.sprite = "A"
+        self.sprite = "X"
         self.passable = False
 
-    def move(self, to='u'):
+    def move(self, map_obj, to='u'):
         if to == 'u':
-            destination = (self.coordinates[0], self.coordinates[1] + 1)
+            dest = (self.coordinates[0], self.coordinates[1] + 1)
         elif to == 'd':
-            destination = (self.coordinates[0], self.coordinates[1] - 1)
+            dest = (self.coordinates[0], self.coordinates[1] - 1)
         elif to == 'l':
-            destination = (self.coordinates[0] - 1, self.coordinates[1])
+            dest = (self.coordinates[0] - 1, self.coordinates[1])
         elif to == 'r':
-            destination = (self.coordinates[0] + 1, self.coordinates[1])
+            dest = (self.coordinates[0] + 1, self.coordinates[1])
         else:
             print('Wrong destination code.')
             return 0
 
-        if False:  # TODO condition why cant move to target point
+        if not map_obj[dest[0]][dest[1]].passable:
             print("Can't move here.")
             return 0
-        self.coordinates = destination
+        self.coordinates = dest
         return 1
 
     def wait(self):
         return 1
 
-    def _find_shelf(self):
-        pass  # TODO write algo to find nearest shelf
+    def _find_shelf(self, map_obj):
+        neighbours_coordinates = [
+            (self.coordinates[0] + 1, self.coordinates[1]),
+            (self.coordinates[0] - 1, self.coordinates[1]),
+            (self.coordinates[0], self.coordinates[1] + 1),
+            (self.coordinates[0], self.coordinates[1] - 1),
+        ]
 
-    def put_product(self, product_name):
-        shelf = self._find_shelf()
+        for (i, j) in neighbours_coordinates:
+            if map_obj[i][j].origin == 'shelf' or map_obj[i][j].origin == 'pickpoint':
+                return map_obj[i][j]
+        return 0
+
+    def put_product(self, product_name, map_obj):
+        shelf = self._find_shelf(map_obj)
         if product_name not in self.inventory or self.inventory[product_name]['count'] == 0:
             print('No such a product.')
             return 0
@@ -115,21 +125,21 @@ class Agent(object):
         self.inventory[product.name]['count'] += 1
         return 1
 
-    def inspect_shelf(self):
-        shelf = self._find_shelf()
+    def inspect_shelf(self, map_obj):
+        shelf = self._find_shelf(map_obj)
         return shelf.inspect()
 
 
 class PickPoint(Shelf):
 
-    def __init__(self, coordinates, max_volume, max_weight):
-        super().__init__(coordinates, max_volume, max_weight)
+    def __init__(self, coordinates):
+        super().__init__(coordinates, max_volume=None, max_weight=None)
         self.origin = "pickpoint"
         self.sprite = "$"
         self.passable = False
 
     def put_product(self, product):
-        return 1
+        return 10
 
     def remove_product(self, product_name):
         return 0
@@ -137,16 +147,20 @@ class PickPoint(Shelf):
     def inspect(self):
         return 0
 
+
 class SimpleFloor(object):
 
     def __init__(self, coordinates):
         self.coordinates = coordinates
         self.passable = True
+        self.origin = "floor"
         self.sprite = "."
+
 
 class Wall(object):
 
     def __init__(self, coordinates):
         self.coordinates = coordinates
         self.passable = False
+        self.origin = "wall"
         self.sprite = "+"
